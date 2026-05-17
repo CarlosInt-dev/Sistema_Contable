999f109906400da2e47c841c49cce7d74b5d5012
Carlos Daniel Interiano Estrada <ie25001@ues.edu.sv>
2026-05-17 11:28:47 -0600

    Prueba concluida

--- src/module_3/Class/HolaMundo.java
+++ src/module_3/Class/HolaMundo.java
@@ -2,6 +2,6 @@ package module_3.Class;
 
 public class HolaMundo {
     static void main() {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo desde carpeta trabajo");
     }
 }
