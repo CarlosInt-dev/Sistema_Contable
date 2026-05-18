b6b8ebda8ea74c5ac92b56ba459643ddf9b08703
Carlos Daniel Interiano Estrada <ie25001@ues.edu.sv>
2026-05-17 21:34:11 -0600

    Cambio en carpeta

--- src/module_3/Class/HolaMundo.java
+++ src/module_3/Class/HolaMundo.java
@@ -3,5 +3,6 @@ package module_3.Class;
 public class HolaMundo {
     static void main() {
         System.out.println("Hola mundo desde carpeta trabajo");
+        System.out.println("Cambio");
     }
 }
